702abb9370ab58cfee65567158fe17c1feb42c28
Satria Aji Putra <satmaxt@outlook.com>
2020-01-04 05:33:15 +0700

    Initial commit

---
+++ src/my/id/satria/latihanmvc/event/PelangganListener.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.id.satria.latihanmvc.event;
+
+import my.id.satria.latihanmvc.model.PelangganModel;
+
+/**
+ *
+ * @author satmaxt
+ */
+public interface PelangganListener {
+    
+    public void onChange(PelangganModel pelanggan);
+    
+}
---
+++ src/my/id/satria/latihanmvc/model/LatihanMVC.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.id.satria.latihanmvc.model;
+
+import my.id.satria.latihanmvc.view.PelangganView;
+
+/**
+ *
+ * @author
+ * NAMA     : Satria Aji Putra
+ * KELAS    : IF-2
+ * NIM      : 10118068
+ * Deskripsi Program : Membuat aplikasi desktop dengan konsep MVC
+ */
+public class LatihanMVC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PelangganView pv = new PelangganView();
+        pv.setVisible(true);
+    }
+    
+}
